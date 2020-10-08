e601711298215f98a08c48a71523b0f5a07a91e7
bryan <bryanmccoy04@gmail.com>
2020-10-07 20:39:26 -0500

    add/edit league pages

---
+++ src/Ui/Pages/Leagues/League.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ui.Pages.Leagues
+{
+    public class AddLeagueModel
+    {
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
